62e75821758a60c582104e2c319ff883e6fb7ec8
Alexey <alopar632@gmail.com>
2025-07-16 17:29:16 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -51,7 +51,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to=movie_image_path)
+    image = models.ImageField(upload_to='uploads/movies/', null=True, blank=True)
 
     class Meta:
         ordering = ["title"]
